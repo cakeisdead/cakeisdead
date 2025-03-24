a20dc6e38c5b234cf75f99f93470747cb95e8f87
cakeisdead <1744036+cakeisdead@users.noreply.github.com>
2025-03-23 19:59:23 -0600

    asdfdg

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,13 +17,15 @@ describe('AppComponent', () => {
   it(`should have the 'cakeisdead' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('cakeisdead');
+    expect(app.title).toEqual('Oscar Lopez');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, cakeisdead');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      "Hey, I'm Oscar!"
+    );
   });
 });
